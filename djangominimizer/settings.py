bd09dfd0153c9acade7911b8e6cfa720e792b9e2
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-26 18:02:04 +0200

    added new variables for minimizer: SCRIPTS and DEBUG.

--- djangominimizer/settings.py
+++ djangominimizer/settings.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+MINIMIZER_DEBUG = getattr(settings, 'MINIMIZER_DEBUG', settings.DEBUG)
+SCRIPTS = getattr(settings, 'MINIMIZER_SCRIPTS', [])
 JQUERY_VERSION = getattr(settings, 'MINIMIZER_JQUERY_VERSION', '1.7')
 DESCRIPTION = getattr(settings, 'MINIMIZER_DESCRIPTION', '')
 AUTHOR = getattr(settings, 'MINIMIZER_AUTHOR', '')
