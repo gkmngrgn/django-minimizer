09914e0b6c8b55e6d55bced5855b46e2ca3c4132
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-26 19:45:11 +0200

    new minimizer setting: CLOSURE_PATH

--- djangominimizer/settings.py
+++ djangominimizer/settings.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 MINIMIZER_DEBUG = getattr(settings, 'MINIMIZER_DEBUG', settings.DEBUG)
+CLOSURE_PATH = getattr(settings, 'MINIMIZER_CLOSURE_PATH', '')
 SCRIPTS = getattr(settings, 'MINIMIZER_SCRIPTS', [])
 JQUERY_VERSION = getattr(settings, 'MINIMIZER_JQUERY_VERSION', '1.7')
 DESCRIPTION = getattr(settings, 'MINIMIZER_DESCRIPTION', '')
