9ffc4d1dc3cf1d71fe5ce20f41f85918c29163e4
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-26 18:02:28 +0200

    added minimizer models for keeping compressed script name.

--- djangominimizer/models.py
+++ djangominimizer/models.py
@@ -1,3 +1,19 @@
+from time import mktime
 from django.db import models
 
-# Create your models here.
+
+class Minimizer(models.Model):
+    script_name = models.CharField(max_length=128, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.script_name
+
+    def save(self, *args, **kwargs):
+        if not self.script_name:
+            self.script_name = mktime(self.created_at.timetuple())
+
+        return super(Minimizer, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('-created_at',)
