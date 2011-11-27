78c9b45f2fe97e0d1d9ed7a1cd062b4dbc0d23d1
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-27 03:08:11 +0200

    added new definition: STYLES.

--- djangominimizer/settings.py
+++ djangominimizer/settings.py
@@ -4,6 +4,7 @@ from django.conf import settings
 MINIMIZER_DEBUG = getattr(settings, 'MINIMIZER_DEBUG', settings.DEBUG)
 CLOSURE_PATH = getattr(settings, 'MINIMIZER_CLOSURE_PATH', '')
 SCRIPTS = getattr(settings, 'MINIMIZER_SCRIPTS', [])
+STYLES = getattr(settings, 'MINIMIZER_STYLES', [])
 JQUERY_VERSION = getattr(settings, 'MINIMIZER_JQUERY_VERSION', '1.7')
 DESCRIPTION = getattr(settings, 'MINIMIZER_DESCRIPTION', '')
 AUTHOR = getattr(settings, 'MINIMIZER_AUTHOR', '')
