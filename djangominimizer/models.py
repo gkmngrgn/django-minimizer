f2da09533ec8ef15031e11b4709c43d648d03f5d
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-27 03:02:01 +0200

    added timestamp instead of script_name

--- djangominimizer/models.py
+++ djangominimizer/models.py
@@ -1,20 +1,16 @@
-from datetime import datetime
 from time import mktime
 from django.db import models
 
 
 class Minimizer(models.Model):
-    script_name = models.CharField(max_length=128, blank=True)
-    created_at = models.DateTimeField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-        return self.script_name
+        return str(self.timestamp)
 
-    def save(self, *args, **kwargs):
-        if not self.script_name:
-            self.created_at = datetime.now()
-            self.script_name = str(int(mktime(self.created_at.timetuple())))
-        return super(Minimizer, self).save(*args, **kwargs)
+    @property
+    def timestamp(self):
+        return int(mktime(self.created_at.timetuple()))
 
     class Meta:
         ordering = ('-created_at',)
