99869263aa0fc28096184d7c0e8964b730808771
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-27 03:03:37 +0200

    create new template tag for getting styles.

--- djangominimizer/templatetags/minimizer_tags.py
+++ djangominimizer/templatetags/minimizer_tags.py
@@ -7,6 +7,16 @@ from djangominimizer.utils import get_minimizer_list
 register  = Library()
 
 
+@register.inclusion_tag('minimizer/tags/styles.html', takes_context=True)
+def minimizer_styles(context):
+    arguments = {
+        'STATIC_URL': context['STATIC_URL'],
+        'scripts': get_minimizer_list(settings.STYLES)
+    }
+
+    return arguments
+
+
 @register.inclusion_tag('minimizer/tags/javascripts.html', takes_context=True)
 def minimizer_javascripts(context):
     arguments = {
