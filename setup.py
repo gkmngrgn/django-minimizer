fb07f2fc2b534a2438df3a1a8e710c8905e9a760
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-27 16:16:01 +0200

    added setup.py installation script.

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.1-alpha'
+DESCRIPTION = """\
+Django application for minimizing static files and using simple html5 template
+with jQuery and modernizr."""
+
+setup(
+    name='django-minimizer',
+    version=VERSION,
+    description=DESCRIPTION,
+    url='http://django-minimizer.alageek.com',
+    author='Gökmen Görgen',
+    author_email='gokmen@alageek.com',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPLv3',
+    keywords='django application html5 yui compressor jquery modernizr',
+)
