589e613d10120b925c304d065b01a77ea374914c
Gökmen Görgen <gkmngrgn@gmail.com>
2011-11-27 03:03:10 +0200

    use get_minimizer_list function in minimizer_tags.

--- djangominimizer/templatetags/minimizer_tags.py
+++ djangominimizer/templatetags/minimizer_tags.py
@@ -1,7 +1,8 @@
 import os
 from django.template import Library
-from djangominimizer.models import Minimizer
 from djangominimizer import settings
+from djangominimizer.models import Minimizer
+from djangominimizer.utils import get_minimizer_list
 
 register  = Library()
 
@@ -10,23 +11,7 @@ register  = Library()
 def minimizer_javascripts(context):
     arguments = {
         'STATIC_URL': context['STATIC_URL']
+        'scripts': get_minimizer_list(settings.SCRIPTS)
     }
-
-    if not settings.MINIMIZER_DEBUG:
-        try:
-            minimizer = Minimizer.objects.latest()
-            arguments.update({'scripts': []})
-
-            for script in settings.SCRIPTS:
-                script_min = ('-%s' % minimizer.script_name).join(
-                    os.path.splitext(script))
-                arguments.get('scripts').append(script_min)
-
-                return arguments
-
-        except Minimizer.DoesNotExist:
-            pass
-
-    arguments.update({'scripts': settings.SCRIPTS})
 
     return arguments
