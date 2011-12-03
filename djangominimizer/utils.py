e8085f779a40feae5f8ad1384b4558bc788b32ff
Gökmen Görgen <gkmngrgn@gmail.com>
2011-12-04 01:47:59 +0200

    fixed some errors about production mode.

--- djangominimizer/utils.py
+++ djangominimizer/utils.py
@@ -1,13 +1,13 @@
+import os
 from djangominimizer import settings
 
-
-def get_minimizer_list(file_list, timestamp):
+def get_minimizer_list(file_list, timestamp, ext):
     if not settings.MINIMIZER_DEBUG:
         file_min_list = []
 
         for file_orig in file_list:
-            file_min = ('-%s' % timestamp).join(
-                os.path.splitext(script))
+            filename = os.path.splitext(file_orig)[0]
+            file_min = '%s-%s.%s' % (filename, timestamp, ext)
             file_min_list.append(file_min)
 
         return file_min_list
